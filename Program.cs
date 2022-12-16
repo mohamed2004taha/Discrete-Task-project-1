c804e96b19b412bb75d7ddbccaf6cca45b4beba1
mohamed2004taha <120724103+mohamed2004taha@users.noreply.github.com>
2022-12-16 15:43:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number:");
+            int Num = int.Parse(Console.ReadLine());
+            for (int i = 2; i < Num / 2; i++)
+            {
+                if (Num % i == 0)
+                {
+                    Console.WriteLine("This number is not prime");
+                    return;
+                }
+            }Console.WriteLine("This number is  prime");
+        }
+       
+    }
+}  
